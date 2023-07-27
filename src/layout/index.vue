9e7bb54780475a29c6136384f3b1dfa9e543a2b9
yunhaoxie <yunhaoxie_99@163.com>
2023-07-27 20:20:39 +0800

    feat: logo的封装

---
+++ src/layout/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="layout_container">
+    <!-- 左侧菜单 -->
+    <div class="layout_slider">
+      <Logo></Logo>
+    </div>
+    <!-- 顶部导航 -->
+    <div class="layout_tabbar">456</div>
+    <!-- 内容展示区域 -->
+    <div class="layout_main">
+      <p style="height: 10000px; background: #000"></p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+//引入左侧菜单logo子组件
+import Logo from './logo/index.vue'
+</script>
+
+<style scoped lang="scss">
+.layout_container {
+  width: 100%;
+  height: 100vh;
+  .layout_slider {
+    width: $base-menu-width;
+    height: 100vh;
+    background: $base-menu-background;
+  }
+  .layout_tabbar {
+    position: fixed;
+    top: 0;
+    left: $base-menu-width;
+    width: calc(100% - $base-menu-width);
+    height: $base-tabbar-height;
+    background: white;
+  }
+  .layout_main {
+    position: absolute;
+    left: $base-menu-width;
+    top: $base-tabbar-height;
+    width: calc(100% - $base-menu-width);
+    height: calc(100vh - $base-tabbar-height);
+    background: gray;
+    padding: 20px;
+    overflow: auto;
+  }
+}
+</style>

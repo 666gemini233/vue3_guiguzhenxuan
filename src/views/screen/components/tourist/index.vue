7c4b76e96691f5d15360adab7b9955e74b8b1ecb
yunhaoxie <yunhaoxie_99@163.com>
2023-08-16 21:49:34 +0800

    feat: 水球图完成

---
+++ src/views/screen/components/tourist/index.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="box">
+    <div class="top">
+      <p class="title">实时游客统计</p>
+      <p class="bg"></p>
+      <p class="right">
+        可预约总量
+        <span>99999</span>
+        人
+      </p>
+    </div>
+    <div class="number">
+      <span v-for="(item, index) in people" :key="index">{{ item }}</span>
+    </div>
+    <!-- 盒子将来echarts展示图形图表的节点 -->
+    <div class="charts" ref="charts">123</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import * as echarts from 'echarts'
+import 'echarts-liquidfill'
+let people = ref('216908人')
+let charts = ref()
+onMounted(() => {
+  //获取echarts实例
+  let mycharts = echarts.init(charts.value)
+  //设置实例的配置项
+  mycharts.setOption({
+    //标题组件
+    title: {
+      text: '',
+    },
+    //x轴组件
+    xAxis: {},
+    //y轴组件
+    yAxis: {},
+    //系列：决定展示什么样的图形图标
+    series: {
+      type: 'liquidFill',
+      data: [0.6],
+      waveAnimation: true,
+      radius: '80%', //半径
+      outline: {
+        borderDistance: 0,
+        itemStyle: {
+          borderWidth: 5,
+          borderColor: '#156ACF',
+          shadowBlur: 20,
+          shadowColor: 'rgba(255, 0, 0, 1)',
+        },
+      },
+    },
+    //布局组件
+    grid: {
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+    },
+  })
+})
+</script>
+
+<style scoped lang="scss">
+.box {
+  background: url(../../images/dataScreen-main-lb.png) no-repeat;
+  background-size: 100% 100%;
+  margin-top: 10px;
+  .top {
+    margin-left: 20px;
+    .title {
+      color: white;
+      font-size: 20px;
+    }
+    .bg {
+      width: 68px;
+      height: 7px;
+      background: url(../../images/dataScreen-title.png) no-repeat;
+      background-size: 100% 100%;
+      margin-top: 10px;
+    }
+    .right {
+      float: right;
+      color: white;
+      font-size: 20px;
+      span {
+        color: orange;
+      }
+    }
+  }
+  .number {
+    margin: 50px 20px 0 20px;
+    display: flex;
+    span {
+      flex: 1;
+      height: 40px;
+      background: url(../../images/total.png) no-repeat;
+      background-size: 100% 100%;
+      text-align: center;
+      line-height: 40px;
+      color: cyan;
+      font-size: 30px;
+    }
+  }
+  .charts {
+    width: 100%;
+    height: 250px;
+  }
+}
+</style>
--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,9 +1,90 @@
 <template>
-  <div>
-    <h1>我是数据大屏一级路由组件</h1>
+  <div class="container">
+    <!-- 数据大屏展示内容区域 -->
+    <div class="screen" ref="screen">
+      <!-- 数据大屏顶部 -->
+      <div class="top">
+        <Top />
+      </div>
+      <div class="bottom">
+        <div class="left">
+          <Tourist class="tourist"></Tourist>
+          <Sex class="sex"></Sex>
+          <Age class="age"></Age>
+        </div>
+        <div class="center">中间</div>
+        <div class="right">右侧</div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+//引入顶部子组件
+import Top from './components/top/index.vue'
+//引入左侧三个子组件
+import Age from './components/age/index.vue'
+import Sex from './components/sex/index.vue'
+import Tourist from './components/tourist/index.vue'
+//获取数据大屏展示内容的盒子
+let screen = ref<any>()
+onMounted(() => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
+})
+//定义大屏缩放比例
+function getScale(w = 1920, h = 1080) {
+  const ww = window.innerWidth / w
+  const wh = window.innerHeight / h
+  return ww < wh ? ww : wh
+}
+//监听视口变化
+window.onresize = () => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
+}
+</script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.container {
+  width: 100vw;
+  height: 100vh;
+  background: url(./images/bg.png) no-repeat;
+  background-size: cover;
+  .screen {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    width: 1920px;
+    height: 1080px;
+    transform-origin: left top;
+    .top {
+      width: 100%;
+      height: 40px;
+    }
+    .bottom {
+      display: flex;
+      .left {
+        flex: 1;
+        height: 1040px;
+        display: flex;
+        flex-direction: column;
+        .tourist {
+          flex: 1.2;
+        }
+        .sex {
+          flex: 1;
+        }
+        .age {
+          flex: 1;
+        }
+      }
+      .center {
+        flex: 2;
+      }
+      .right {
+        flex: 1;
+      }
+    }
+  }
+}
+</style>
